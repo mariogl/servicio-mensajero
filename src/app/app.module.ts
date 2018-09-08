d93d57c729653b117f9d1f17752ad1442c0ef53d
Mario González <mgonzalezle@gmail.com>
2018-09-08 10:08:29 +0200

    Componente App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemporizadorComponent } from './temporizador/temporizador.component';
+import { AvisosComponent } from './avisos/avisos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemporizadorComponent,
+    AvisosComponent
   ],
   imports: [
     BrowserModule
