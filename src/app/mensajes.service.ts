930fc935919681f9160d0ee3284ab95209b0b720
Mario González <mgonzalezle@gmail.com>
2018-09-08 16:51:27 +0200

    Corrección tabulados

--- src/app/mensajes.service.ts
+++ src/app/mensajes.service.ts
@@ -4,30 +4,30 @@ import { filter } from 'rxjs/operators'; // Tenemos que importar el operador fil
 
 // Interfaz para la forma del objeto mensaje
 interface Mensaje {
-	tema: string;
-	contenido: string;
+  tema: string;
+  contenido: string;
 }
 
 @Injectable({
-	providedIn: 'root' // Así se establece a partir de Angular 6 el ámbito de la instancia del servicio
+  providedIn: 'root' // Así se establece a partir de Angular 6 el ámbito de la instancia del servicio
 })
 export class MensajesService {
-	private mensajero: BehaviorSubject<Mensaje> = new BehaviorSubject({
-		tema: '',
-		contenido: ''
+  private mensajero: BehaviorSubject<Mensaje> = new BehaviorSubject({
+    tema: '',
+    contenido: ''
   });
 
-	constructor() { }
+  constructor() { }
 
-	// Método público para quien se quiera suscribir a los mensajes
-	public escucha(tema: string): Observable<Mensaje> {
-		return this.mensajero.asObservable().pipe(
+  // Método público para quien se quiera suscribir a los mensajes
+  public escucha(tema: string): Observable<Mensaje> {
+    return this.mensajero.asObservable().pipe(
       filter(msj => msj.tema === tema)
     );
-	}
+  }
 
-	// Método público para quien quiera emitir un mensaje
-	public emite(msj: Mensaje): void {
-		this.mensajero.next(msj);
-	}
+  // Método público para quien quiera emitir un mensaje
+  public emite(msj: Mensaje): void {
+    this.mensajero.next(msj);
+  }
 }
