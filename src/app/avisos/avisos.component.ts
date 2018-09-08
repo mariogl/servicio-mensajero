9d11130d49acfbb8251de8c22a0f9ca2a4b9fd05
Mario González <mgonzalezle@gmail.com>
2018-09-08 12:16:59 +0200

    Cancelación de suscripciones

--- src/app/avisos/avisos.component.ts
+++ src/app/avisos/avisos.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MensajesService } from '../mensajes.service';  // Nuestro proveedor de mensajes
+import { Subscription } from 'rxjs';  // Importamos el objeto Subscription de rxjs
 
 @Component({
   selector: 'msjs-avisos',
   templateUrl: './avisos.component.html',
   styleUrls: ['./avisos.component.css']
 })
-export class AvisosComponent implements OnInit {
+export class AvisosComponent implements OnInit, OnDestroy {
   public aviso = '';
+  private suscripcionMensajes: Subscription; // Aquí almacenaremos la suscripción
 
   constructor(private mensajesService: MensajesService) { }  // Inyectamos nuestro servicio de mensajes
 
   private escuchaMensajes(): void {
-    this.mensajesService.escucha('tiempo').subscribe(  // Al llamar a escucha() le pasamos el tema que nos interesa
+    // tslint:disable-next-line:max-line-length
+    this.suscripcionMensajes = this.mensajesService.escucha('tiempo').subscribe(  // Al llamar a escucha() le pasamos el tema que nos interesa
       msj => {
         switch (msj.contenido) {
           case 'inicio':
@@ -35,5 +38,9 @@ export class AvisosComponent implements OnInit {
 
   ngOnInit(): void {
     this.escuchaMensajes();
+  }
+
+  ngOnDestroy(): void {
+    this.suscripcionMensajes.unsubscribe();  // Cancelamos la suscripción cuando se destruya el componente
   }
 }
